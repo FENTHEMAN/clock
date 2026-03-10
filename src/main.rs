9b3330668a7da5b43fb976cbe67af8cd3168d590
fen <fenfenus@MacBook-Air-fen.local>
2026-03-11 00:39:31 +0700

    [FEATURE] Add 'clap' functionality to client usabilty (only get time with standarts)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,60 @@
-use chrono::Local;
+use chrono::{DateTime, Local};
+use clap::{Parser, ValueEnum};
+
+struct Clock;
+
+impl Clock {
+    fn get() -> DateTime<Local> {
+        Local::now()
+    }
+
+    fn set() -> ! {
+        unimplemented!()
+    }
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum Action {
+    Set,
+    Get,
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum Standart {
+    Timestamp,
+    RFC2822,
+    RFC3339,
+}
+
+#[derive(Parser)]
+#[command(name = "clock", about = "Time client", version = "0.1.1")]
+struct App {
+    #[arg(default_value = "get")]
+    action: Action,
+
+    #[arg(help = "When [ACTION] is setted to 'set', apply [DATETIME]. \
+    Otherwise, ignore.")]
+    datetime: Option<String>,
+
+    #[arg(long = "use-standart", short = 's', default_value = "timestamp")]
+    use_standart: Standart,
+}
 
 fn main() {
-    let now = Local::now();
+    let app = App::parse();
 
-    println!("now: {}", now);
+    match app.action {
+        Action::Get => {
+            let now = Clock::get();
+            println!(
+                "{}",
+                match app.use_standart {
+                    Standart::Timestamp => now.timestamp().to_string(),
+                    Standart::RFC2822 => now.to_rfc2822(),
+                    Standart::RFC3339 => now.to_rfc3339(),
+                }
+            );
+        }
+        Action::Set => unimplemented!(),
+    }
 }
